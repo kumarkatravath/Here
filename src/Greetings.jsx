918fc30c41a14f93540788e1093cd440e598d2a3
saiiyen <kumarkatravath.01@gmail.com>
2025-03-17 20:28:39 +0530

    method

--- src/Greetings.jsx
+++ src/Greetings.jsx
@@ -8,3 +8,11 @@ const Greetings = (props) => {
 }
 
 export default Greetings
+
+// or the another way of doing what u do
+// const Greetings = ({timeofDay}) => {
+//     return  timeofDay==='morning' ?(<h1>Good morning</h1>) : (<h1>Good Afternoon</h1>)
+ 
+//  }
+ 
+//  export default Greetings
